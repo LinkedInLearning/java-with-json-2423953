a59e112d7aa4f0e6a7370a81fc5b78a356e0a530
JL West <jonluke@homeseer.com>
2021-12-02 03:07:36 -0500

    03_06e

--- src/main/java/com/example/jsonnotes/notes/JsonNotesFile.java
+++ src/main/java/com/example/jsonnotes/notes/JsonNotesFile.java
@@ -1,19 +1,25 @@
 package com.example.jsonnotes.notes;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class JsonNotesFile {
 
-    private List<Note> notes = new ArrayList<>();
+    private Map<String, Note> notes = new HashMap<>();
 
     public JsonNotesFile() {}
 
     public List<Note> getNotes() {
-        return notes;
+        return notes.values().stream().toList();
     }
 
     public void setNotes(List<Note> notes) {
-        this.notes = notes;
+        var noteMap = new HashMap<String, Note>();
+        for (var note : notes) {
+            noteMap.put(note.getId(), note);
+        }
+        this.notes = noteMap;
     }
 }
