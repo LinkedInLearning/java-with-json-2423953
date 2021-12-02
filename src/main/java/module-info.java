2204389da11b17de47c6caa176a4b0ddf437f3df
JL West <jonluke@homeseer.com>
2021-12-02 02:59:49 -0500

    02_03e

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,13 @@ module com.example.jsonnotes {
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.desktop;
+    requires com.google.gson;
 
 
     opens com.example.jsonnotes to javafx.fxml;
     exports com.example.jsonnotes;
     exports com.example.jsonnotes.notes;
-    opens com.example.jsonnotes.notes to javafx.fxml;
+    opens com.example.jsonnotes.notes to javafx.fxml, com.google.gson;
     exports com.example.jsonnotes.notes.service;
     opens com.example.jsonnotes.notes.service to javafx.fxml;
 }
