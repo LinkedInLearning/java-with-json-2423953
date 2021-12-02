e622334fcf119f2930f55bdd0f0d3980b95d7fe2
JL West <jonluke@homeseer.com>
2021-12-02 03:00:36 -0500

    02_04e

---
+++ src/main/java/com/example/jsonnotes/notes/service/JsonNoteDataService.java
@@ -0,0 +1,50 @@
+package com.example.jsonnotes.notes.service;
+
+import com.example.jsonnotes.notes.Note;
+import com.google.gson.Gson;
+
+/**
+ * A data service for {@link Note Notes} that stores all data in individual text files that only contain the content of the {@link Note}.
+ */
+public class JsonNoteDataService extends FileNoteDataService {
+    /**
+     * Single instance of {@link JsonNoteDataService}
+     */
+    private static JsonNoteDataService _instance;
+
+    /**
+     * Create a new single instance of {@link JsonNoteDataService}
+     * @param dataPath Absolute path to the directory where {@link Note notes} are stored
+     * @return {@link JsonNoteDataService}
+     * @throws IllegalArgumentException Thrown when no dataPath is specified and no {@link JsonNoteDataService#_instance} is available
+     */
+    public static JsonNoteDataService getInstance(String dataPath) throws IllegalArgumentException {
+        if (dataPath != null) {
+            //When a dataPath is provided, generate a new instance
+            _instance = new JsonNoteDataService(dataPath);
+        }
+        if (_instance == null) {
+            //When no dataPath is specified and there is no current instance, we cannot continue.
+            throw new IllegalArgumentException("No instance available and no dataPath specified. Cannot proceed.");
+        }
+        return _instance;
+    }
+
+    /**
+     * Create a new instance of a {@link JsonNoteDataService} pointing at a specific path
+     */
+    public JsonNoteDataService(String dataPath) {
+        super(dataPath);
+    }
+
+    @Override
+    protected Note getNoteFromFileData(String fileData) {
+        return new Note(fileData);
+    }
+
+    @Override
+    protected String getFileDataFromNote(Note note) {
+        var gson = new Gson();
+        return gson.toJson(note);
+    }
+}
